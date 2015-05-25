2b8ef74bc953b170a95c5daf8c887633e1da2b01
Arab Source <info@arabsource.org>
2015-05-25 02:26:29 -0400

    the code for First java class

---
+++ EclipseCode/JavaByProjects/src/Lesson_001.java
@@ -0,0 +1,10 @@
+
+public class Lesson_001 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("www.arabsource.org");
+
+	}
+
+}
---
+++ NetBeansCode/JavaByProjects/src/Lesson_001.java
@@ -0,0 +1,12 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+public class Lesson_001 {
+    public static void main(String [] args){
+        System.out.println("Hello World");
+        System.out.println("www.arabsource.org");
+    }
+}
