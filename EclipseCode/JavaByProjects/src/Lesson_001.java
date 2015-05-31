ebd1d429e51ad94c2614bbfe284e0749c59dc365
Arab Source <info@arabsource.org>
2015-05-31 19:55:17 -0400

    upload the second lesson of Learn java programming by projects

--- EclipseCode/JavaByProjects/src/Lesson_001.java
+++ EclipseCode/JavaByProjects/src/Lesson_001.java
@@ -1,10 +1,15 @@
+/*
+   Copyright 2015 ArabSource
+   Project : Learn Java By Projects
+   Lesson : 1
+   Date : May 25th, 2015
+ */
 
 public class Lesson_001 {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("www.arabsource.org");
-
 	}
 
 }
---
+++ EclipseCode/JavaByProjects/src/Lesson_002.java
@@ -0,0 +1,42 @@
+/*
+   Copyright 2015 ArabSource
+   Project : Learn Java By Projects
+   Lesson : 2
+   Date : May 31th, 2015
+ */
+
+public class Lesson_002 {
+
+	public static void main (String [] args){
+		//veriable_type   variable_name = value;
+		byte byteVar = 13;
+		short shortVar = 130;
+		int intVar = 56000;
+		long longVar = 1578999000;
+		
+		float floatVar = 5.67f;
+		double doubleVar = 6.457;
+		
+		char charVar = 'A';
+		String strVar = "www.arabsource.org";
+		
+		boolean booleanVar = false;
+		
+		int x = 13;
+		int y = 3;
+		int z = 0;
+		
+		z = x + y;
+		z = x - y;
+		z = x * y;
+		z = x / y;
+		z = x % y;
+		
+		x++;
+		x--;
+		
+		System.out.println("z = " + z);
+	
+	}
+	
+}
