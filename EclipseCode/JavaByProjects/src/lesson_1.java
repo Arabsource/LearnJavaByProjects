6eb55664430a21f6f47d41937867492bf4454e54
Arab Source <info@arabsource.org>
2015-06-12 20:30:59 -0400

    Fourth Java class

---
+++ EclipseCode/JavaByProjects/src/lesson_1.java
@@ -0,0 +1,18 @@
+public class lesson_1 {
+	
+	//Class Variable and make aailable to any function and class
+	static String randomString = "String to print";
+	
+	//vairable do not change
+	static final double PINUM = 3.14; 
+	
+	public static void main(String [] arog){
+		System.out.println(randomString);
+		System.out.println(PINUM);
+		
+		int intOne, inttwo;
+		
+		System.out.println(Integer.MAX_VALUE);
+		
+	}
+}
---
+++ EclipseCode/JavaByProjects/src/lesson_3.java
@@ -0,0 +1,31 @@
+
+public class lesson_3 {
+
+	public static void main(String[] args) {
+		int x = 10;
+		int y = 20;
+		
+		boolean chk = true;
+		
+		if( x < y ){
+			System.out.println("X = " + x);	//true
+		}else{
+			System.out.println("Y = " + y);	//false
+		}
+		
+		int z = 200;
+		
+		switch(z){
+		case 100:
+			System.out.println("The number is : "+z);
+			break;
+		case 200:
+			System.out.println("the number is : "+z);
+			System.out.println(z+" is the case number 2"+" correct");
+			break;
+		default :
+			System.out.println("the number is something else");
+		}
+	}
+
+}
